61225393fc5e100f23f5cdb6de4ebe5a271120ca
beavercoffee <beavercoffee@vidsoe.com>
2021-07-11 19:07:45 -0600

    BC

--- bc-cf7-redirect.php
+++ bc-cf7-redirect.php
@@ -12,7 +12,7 @@ Plugin URI: https://github.com/beavercoffee/bc-cf7-redirect
 Requires at least: 5.7
 Requires PHP: 5.6
 Text Domain: bc-cf7-redirect
-Version: 1.7.9.4
+Version: 1.7.9.5
 */
 
 if(defined('ABSPATH')){
